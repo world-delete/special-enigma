85c3ead3bd9cc155f5648f59817d5cbbf33a12ce
world-delete <martin.labule@gmail.com>
2024-12-17 20:20:10 +1000

    Visit Created Lesson 1

---
+++ WorldJava 1/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("****************************");
+        System.out.println("****       Визитка      ****");
+        System.out.println("****      Name: Илья    ****");
+        System.out.println("*** Discord:white_prince ***");
+        System.out.println("****************************");
+
+    }
+}
