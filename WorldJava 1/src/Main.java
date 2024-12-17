2c4ad561d0065f03bdbc0bd26c4ddfaa753dffa2
world-delete <martin.labule@gmail.com>
2024-12-17 20:24:18 +1000

    add symbol escape

--- WorldJava 1/src/Main.java
+++ WorldJava 1/src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("****************************");
-        System.out.println("****       Визитка      ****");
-        System.out.println("****      Name: Илья    ****");
-        System.out.println("*** Discord:white_prince ***");
-        System.out.println("****************************");
+        System.out.print("********************************\n");
+        System.out.print("****        Визитка         ****\n");
+        System.out.print("****      Name: \"Илья\"      ****\n");
+        System.out.print("****  Discord:white_prince  ****\n");
+        System.out.print("********************************\n");
+
 
     }
 }
