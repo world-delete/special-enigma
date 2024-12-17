6bf979b919055a035dad67b9c69375bc5aaabde8
world-delete <martin.labule@gmail.com>
2024-12-17 22:46:42 +1000

    Homework lesson 3

---
+++ WorldJava 3/src/Main.java
@@ -0,0 +1,57 @@
+/*
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+*/
+/*        System.out.println("Введите свой рост (м)");
+        double rost = new Scanner(System.in).nextDouble();
+        System.out.println("Введите свой вес (кг)");
+        double mass = new Scanner(System.in).nextDouble();
+
+        System.out.println("BMI: " + mass / (rost * rost));*//*
+
+
+ */
+/*        System.out.println("\t\t***Добро пожаловать в калькулятор умножения чисел***\n\n");
+        System.out.println("Введите первое число :");
+        int a = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число :");
+        int b = new Scanner(System.in).nextInt();
+
+        System.out.println(a + " * " + b + " = " + a * b);*//*
+
+
+        char s0 = 's';
+
+
+
+    }
+}*/
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+/*        int a = 1;
+        int b = 2;
+        int catTest = 51;
+        int dogTest = 61;
+        int testCodeVar = 39;
+        String s = "some text";
+        char c = 'T';
+        char cc = 'H';
+        int i = 10;
+        double d = 19.0;
+        System.out.println("Значение всех переменных - " + a + " " + b + " " + catTest + " " + dogTest +
+                " " + testCodeVar + " " + s + " " + c + " " + cc + " " + i + " " + d);*/
+
+        System.out.println("\t\t***Добро пожаловать в калькулятор деления чисел***\n\n");
+        System.out.println("Введите первое число :");
+        double a = new Scanner(System.in).nextDouble();
+        System.out.println("Введите второе число :");
+        double b = new Scanner(System.in).nextDouble();
+        System.out.println(a + " * " + b + " = " + a / b);
+
+    }
+}
